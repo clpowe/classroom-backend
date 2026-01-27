52f4cd52bdc72ac0a121352e8771a721e5097490
clpowe <clpowe@gmail.com>
2026-01-26 22:20:48 -0500

    implament security middleware

---
+++ src/config/arcjet.ts
@@ -0,0 +1,21 @@
+import arcjet, { shield, detectBot, slidingWindow } from "@arcjet/node";
+
+if (!process.env.ARCJET_KEY && process.env.NODE_ENV != "test") {
+  throw new Error("ARCJET_KEY environment variable is not set");
+}
+
+export const aj = arcjet({
+  key: process.env.ARCJET_KEY!,
+  rules: [
+    shield({ mode: "LIVE" }),
+    detectBot({
+      mode: "LIVE",
+      allow: ["CATEGORY:SEARCH_ENGINE", "CATEGORY:PREVIEW"],
+    }),
+    slidingWindow({
+      mode: "LIVE",
+      interval: "2s",
+      max: 5,
+    }),
+  ],
+});
