183bb0c8366aaa4472f2b857c2a08b87b5450396
clpowe <clpowe@gmail.com>
2026-01-28 05:34:29 -0500

    Implament Security

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import subjectsRouter from "./routes/subjects";
 import cors from "cors";
+import securityMiddleware from "./middleware/security";
 
 const app = express();
 const PORT = 8000;
@@ -18,6 +19,7 @@ app.use(
 );
 
 app.use(express.json());
+app.use(securityMiddleware);
 
 app.use("/api/subjects", subjectsRouter);
 
